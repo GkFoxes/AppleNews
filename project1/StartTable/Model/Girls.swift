91fc3db27c840ad7eec7ab8325b43eb292e3cfed
GkFoxes <dima25_matveenko@icloud.com>
2018-06-16 21:07:56 +0300

    Add delete cell from table

--- project1/StartTable/Model/Girls.swift
+++ project1/StartTable/Model/Girls.swift
@@ -8,7 +8,36 @@
 
 import Foundation
 
-struct Girls {
-    let name: String
-    let years: Int
-}
+var girls = [
+    "Анджелина Джоли",
+    "Скарлетт Йоханссон",
+    "Меган Фокс",
+    "Шарлиз Терон",
+    "Моника Беллуччи",
+    "Натали Портман",
+    "Мила Кунис",
+    "Кира Найтли",
+    "Дженнифер Лоуренс",
+    "Марго Робби"
+]
+
+//struct Girls {
+//    let name: String
+//    let years: Int
+//}
+//
+//var girls: [Girls] = [
+//    Girls(name: "Анджелина Джоли", years: 43),
+//    Girls(name: "Скарлетт Йоханссон", years: 33),
+//    Girls(name: "Меган Фокс", years: 32),
+//    Girls(name: "Шарлиз Терон", years: 42),
+//    Girls(name: "Моника Беллуччи", years: 53),
+//    Girls(name: "Натали Портман", years: 37),
+//    Girls(name: "Мила Кунис", years: 34),
+//    Girls(name: "Кира Найтли", years: 33),
+//    Girls(name: "Дженнифер Лоуренс", years: 27),
+//    Girls(name: "Марго Робби", years: 27)]
+
+
+
+
