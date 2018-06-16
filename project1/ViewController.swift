026a2893226c3064aa32501389b814ea78845fd3
GkFoxes <dima25_matveenko@icloud.com>
2018-06-16 16:19:21 +0300

    Initial commit

---
+++ project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Дмитрий Матвеенко on 16.06.2018.
+//  Copyright © 2018 Дмитрий Матвеенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
