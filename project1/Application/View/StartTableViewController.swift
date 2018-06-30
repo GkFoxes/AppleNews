b5f8bb28ff44bdbb41071bd0bbb1a6456f031d88
GkFoxes <dima25_matveenko@icloud.com>
2018-06-30 22:47:34 +0300

    Add ten russia city in API

--- project1/Application/View/StartTableViewController.swift
+++ project1/Application/View/StartTableViewController.swift
@@ -134,16 +134,16 @@ class StartTableViewController: UITableViewController, UIPopoverPresentationCont
     
     lazy var weatherManager = APIWeatherManager(apiKey: "416e4d01fc649f94c5c4b5c68ec20ed6")
     let coordinates = [
-        Coordinates(latitude: 54.741704, longitude: 55.984471, name: "St.peters"),
-        Coordinates(latitude: 56.741704, longitude: 52.984471, name: "Moscow"),
-//        Coordinates(latitude: 42.741704, longitude: 38.984471),
-//        Coordinates(latitude: 54.741704, longitude: 55.984471),
-//        Coordinates(latitude: 54.741704, longitude: 55.984471),
-//        Coordinates(latitude: 54.741704, longitude: 55.984471),
-//        Coordinates(latitude: 54.741704, longitude: 55.984471),
-//        Coordinates(latitude: 54.741704, longitude: 55.984471),
-//        Coordinates(latitude: 54.741704, longitude: 55.984471),
-//        Coordinates(latitude: 54.741704, longitude: 55.984471)
+        Coordinates(latitude: 59.939095, longitude: 30.315868, name: "St. Petersburg"),
+        Coordinates(latitude: 55.755814, longitude: 37.617635, name: "Moscow"),
+        Coordinates(latitude: 54.707390, longitude: 20.507307, name: "Kaliningrad"),
+        Coordinates(latitude: 53.195063, longitude: 45.018316, name: "Penza"),
+        Coordinates(latitude: 55.030199, longitude: 82.920430, name: "Novosibirsk"),
+        Coordinates(latitude: 55.796289, longitude: 49.108795, name: "Kazan"),
+        Coordinates(latitude: 58.522810, longitude: 31.269915, name: "Veliky Novgorod"),
+        Coordinates(latitude: 56.326887, longitude: 44.005986, name: "Nizhny Novgorod"),
+        Coordinates(latitude: 54.989342, longitude: 73.368212, name: "Omsk"),
+        Coordinates(latitude: 53.195538, longitude: 50.101783, name: "Samara")
     ]
     
     func fetchCurrentWeatherData(){
@@ -170,7 +170,7 @@ class StartTableViewController: UITableViewController, UIPopoverPresentationCont
     func updateUIWith(currentWeather: CurrentWeather, coordinates: Coordinates ) {
         let girlItem = Girl()
         girlItem.name = coordinates.name
-        girlItem.biography = currentWeather.temperatureString
+        girlItem.biography = currentWeather.pressureString
         girlItem.link = currentWeather.appearentTemperatureString
         
         try! realm.write({
