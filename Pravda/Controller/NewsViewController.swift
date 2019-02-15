17278ce4b24e08bbfcec7da29265f8438e2db5c2
GkFoxes <dima25_matveenko@icloud.com>
2019-02-15 13:49:41 +0300

    Bugfix

--- Pravda/Controller/NewsViewController.swift
+++ Pravda/Controller/NewsViewController.swift
@@ -44,15 +44,14 @@ class NewsViewController: UITableViewController {
     // MARK: - Segues
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDetail" { /*
-            if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
-                let controller = (segue.destination as! UINavigationController).topViewController as! NewsDetailViewController
-                controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
-                controller.navigationItem.leftItemsSupplementBackButton = true
-            }
-            */
+        if segue.identifier == "showDetail" {
+            //if let indexPath = tableView.indexPathForSelectedRow {
+                
+                let destinationViewController = (segue.destination as! UINavigationController).topViewController as! NewsDetailViewController
+                
+                destinationViewController.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                destinationViewController.navigationItem.leftItemsSupplementBackButton = true
+            //}
         }
     }
 
