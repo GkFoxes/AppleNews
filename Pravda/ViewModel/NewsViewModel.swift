f898fe066230aafe310e1ac84ab8939fcd142bd4
GkFoxes <dima25_matveenko@icloud.com>
2019-02-20 12:14:07 +0300

    Delete fake data

--- Pravda/ViewModel/NewsViewModel.swift
+++ Pravda/ViewModel/NewsViewModel.swift
@@ -12,19 +12,16 @@ class NewsViewModel: NewsTableViewViewModelType {
     
     private var selectedIndexPath: IndexPath?
     
-    var news =
-        News(status: "ok", totalResults: 3, articles: [
-            NewsAPI(source: nil, author: "CNN", title: "BREAKING something", description: nil, url: nil, urlToImage: nil, publishedAt: "14:55"),
-            NewsAPI(source: nil, author: "P2P", title: "What is this?", description: nil, url: nil, urlToImage: nil, publishedAt: "12:41"),
-            NewsAPI(source: nil, author: "PostPravda", title: "Okey, but not okey. What are he waiting for? Trouble", description: nil, url: nil, urlToImage: nil, publishedAt: "9:58")
-            ])
+    var news: News?
     
     func numberOfRows() -> Int {
+        guard let news = news else { return 0 }
         guard let articles = news.articles else { return 0 }
         return articles.count
     }
     
     func cellViewModel(forIndexPath indexPath: IndexPath) -> NewsTableViewCellViewModelType? {
+        guard let news = news else { return nil }
         guard let articles = news.articles else { return nil }
         let article = articles[indexPath.row]
         return NewsTableViewCellViewModel(article: article)
@@ -32,6 +29,7 @@ class NewsViewModel: NewsTableViewViewModelType {
     
     func viewModelForSelectedRow() -> NewsDetailViewModelType? {
         guard let selectedIndexPath = selectedIndexPath else { return nil }
+        guard let news = news else { return nil }
         guard let articles = news.articles else { return nil }
         return NewsDetailViewModel(article: articles[selectedIndexPath.row])
     }
