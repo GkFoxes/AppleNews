954474fa4023e755da5dbb7becec0f1aeee18264
GkFoxes <dima25_matveenko@icloud.com>
2019-02-20 11:18:49 +0300

    Add new Models

---
+++ Pravda/Model/Source.swift
@@ -0,0 +1,19 @@
+//
+//  Source.swift
+//  Pravda
+//
+//  Created by Дмитрий Матвеенко on 20/02/2019.
+//  Copyright © 2019 GkFoxes. All rights reserved.
+//
+
+import Foundation
+
+struct Source: Codable {
+    var id: String?
+    var name: String?
+    
+    init(id: String?, name: String?) {
+        self.id = id
+        self.name = name
+    }
+}
---
+++ Pravda/Services/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  Pravda
+//
+//  Created by Дмитрий Матвеенко on 20/02/2019.
+//  Copyright © 2019 GkFoxes. All rights reserved.
+//
+
+import Foundation
