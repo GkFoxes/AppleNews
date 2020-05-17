5bee98e0d20e3335453a982acc6173343d42988c
GkFoxes <dima26tamys@gmail.com>
2020-05-17 17:24:07 +0300

    MVC: Init clean project

---
+++ Pravda/Pravda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pravda
+//
+//  Created by Матвеенко Дмитрий on 17.05.2020.
+//  Copyright © 2020 GkFoxes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
