5aacf71444055d2ded4042417c4e7d3fb2f73747
GkFoxes <dima26tamys@gmail.com>
2020-05-19 17:12:15 +0300

    MVC: Init FavoritesViewController

---
+++ Pravda/Pravda-MVC/Pravda-MVC/Modules/Favorites/FavoritesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritesViewController.swift
+//  Pravda-MVC
+//
+//  Created by Матвеенко Дмитрий on 19.05.2020.
+//  Copyright © 2020 GkFoxes. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
