ba3131dcf23b4f268bb308eeeccc3b4a17a4a6b7
GkFoxes <dima26tamys@gmail.com>
2020-05-27 20:53:29 +0300

    PravdaUIKit: Init SectionsTabBarController

---
+++ Pravda/PravdaUIKit/PravdaUIKit/ViewControllers/SectionsTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  SectionsTabBarController.swift
+//  PravdaUIKit
+//
+//  Created by Дмитрий Матвеенко on 27.05.2020.
+//  Copyright © 2020 GkFoxes. All rights reserved.
+//
+
+import UIKit
+
+class SectionsTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
