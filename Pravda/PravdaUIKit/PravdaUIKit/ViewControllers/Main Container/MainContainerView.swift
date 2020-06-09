ed1dc592178fa95d08f3600db06434d6729886bd
GkFoxes <dima26tamys@gmail.com>
2020-06-09 15:58:20 +0300

    PravdaUIKit: Init MainContainerView

---
+++ Pravda/PravdaUIKit/PravdaUIKit/ViewControllers/Main Container/MainContainerView.swift
@@ -0,0 +1,21 @@
+//
+//  MainContainerView.swift
+//  PravdaUIKit
+//
+//  Created by Матвеенко Дмитрий Владимирович on 08.06.2020.
+//  Copyright © 2020 GkFoxes. All rights reserved.
+//
+
+import UIKit
+
+class MainContainerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
