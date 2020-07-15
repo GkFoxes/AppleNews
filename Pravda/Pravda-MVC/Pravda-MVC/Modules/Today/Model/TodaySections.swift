f3ff620bdd83d99fe250b4175e6e40af5dd11362
GkFoxes <dima26tamys@gmail.com>
2020-07-15 21:16:49 +0300

    Pravda-MVC: Implement TodaySections

---
+++ Pravda/Pravda-MVC/Pravda-MVC/Modules/Today/Model/TodaySections.swift
@@ -0,0 +1,14 @@
+//
+//  TodaySections.swift
+//  Pravda-MVC
+//
+//  Created by Матвеенко Дмитрий Владимирович on 15.07.2020.
+//  Copyright © 2020 GkFoxes. All rights reserved.
+//
+
+import Foundation
+
+enum TodaySections {
+	case topStories
+	case fashion
+}
