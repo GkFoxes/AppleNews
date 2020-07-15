bc2c4ae9310ad3c976ce7f4b44661cb447c7f7a2
GkFoxes <dima26tamys@gmail.com>
2020-07-15 21:17:30 +0300

    Pravda-MVC: Test implement UICollectionViewDiffableDataSource

--- Pravda/Pravda-MVC/Pravda-MVC/Modules/Today/TodayViewController.swift
+++ Pravda/Pravda-MVC/Pravda-MVC/Modules/Today/TodayViewController.swift
@@ -10,6 +10,19 @@ import PravdaUIKit
 
 final class TodayViewController: UIViewController {
 
+	// MARK: Properties
+
+//	private lazy var dataSource: UICollectionViewDiffableDataSource<TodaySections, TodayNewsItem> = {
+//		setupCollectionViewDataSource()
+//	}()
+	private var dataSource: UICollectionViewDiffableDataSource<TodaySections, TodayNewsItem>!
+
+	// MARK: Views
+
+	private lazy var collectionView: UICollectionView = {
+		return UICollectionView(frame: view.bounds, collectionViewLayout: setupCollectionViewCompositionalLayout())
+	}()
+
 	// MARK: Life Cycle
 
 	init() {
@@ -28,16 +41,125 @@ final class TodayViewController: UIViewController {
 	override func loadView() {
 		super.loadView()
 
-		self.view.backgroundColor = .red
 		self.navigationController?.setupBlackDesignAppearances()
+
+		setupCollectionViewAppearances()
+		setupCollectionViewLayout()
+		setupCollectionViewDataSource()
 	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-//		if let splitController = self.splitViewController {
-//			self.navigationItem.leftBarButtonItem = splitController.displayModeButtonItem
-//		}
+		/*
+		if let splitController = self.splitViewController {
+			self.navigationItem.leftBarButtonItem = splitController.displayModeButtonItem
+		}*/
+	}
+}
+
+// MARK: Collection View Delegate
+
+extension TodayViewController: UICollectionViewDelegate {
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//		guard let item = dataSource.itemIdentifier(for: indexPath) else { return }
+//		let photoDetailVC = PhotoDetailViewController(photoURL: item.photoURL)
+//		navigationController?.pushViewController(photoDetailVC, animated: true)
+	}
+}
+
+// MARK: Setup Collection Appearances
+
+private extension TodayViewController {
+	func setupCollectionViewAppearances() {
+		collectionView.delegate = self
+		collectionView.register(
+			TitleNewsTopicCollectionViewCell.self,
+			forCellWithReuseIdentifier: TitleNewsTopicCollectionViewCell.reuseIdentifer
+		)
+
+		collectionView.backgroundColor = .systemBackground
+	}
+}
+
+// MARK: Setup Collection Layout
+
+private extension TodayViewController {
+	func setupCollectionViewLayout() {
+		view.addSubview(collectionView)
+		collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+		NSLayoutConstraint.activate([
+			collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+
+			collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+			collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+		])
+	}
+
+	func setupCollectionViewCompositionalLayout() -> UICollectionViewLayout {
+		let titleNewsTopicItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(
+			widthDimension: .fractionalWidth(1.0),
+			heightDimension: .fractionalHeight(1.0)
+		))
+
+		titleNewsTopicItem.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16)
+
+		let topStoriesGroup = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(
+			widthDimension: .fractionalWidth(1.0),
+			heightDimension: .fractionalHeight(1/3)
+		), subitems: [titleNewsTopicItem])
+
+		let section = NSCollectionLayoutSection(group: topStoriesGroup)
+		let layout = UICollectionViewCompositionalLayout(section: section)
+		return layout
+	}
+}
+
+// MARK: Setup Data Source
+
+private extension TodayViewController {
+	func setupCollectionViewDataSource() {
+		dataSource = UICollectionViewDiffableDataSource<TodaySections, TodayNewsItem>(
+			collectionView: collectionView
+		) { (collectionView: UICollectionView, indexPath: IndexPath, detailItem: TodayNewsItem)
+			-> UICollectionViewCell? in
+			guard let titleNewsTopicCell = collectionView.dequeueReusableCell(
+				withReuseIdentifier: TitleNewsTopicCollectionViewCell.reuseIdentifer,
+				for: indexPath
+				) as? TitleNewsTopicCollectionViewCellProtocol
+			else {
+				assertionFailure("Couldn't create TitleNewsTopic Cell")
+				return UICollectionViewCell()
+			}
+
+			titleNewsTopicCell.setupContent(
+				image: nil,
+				source: detailItem.source,
+				title: detailItem.title,
+				timePublication: detailItem.timePublication
+			)
+			return titleNewsTopicCell
+		}
+
+		let snapshot = getCurrentStateSnapshot()
+		dataSource.apply(snapshot, animatingDifferences: true)
+	}
+
+	func getCurrentStateSnapshot() -> NSDiffableDataSourceSnapshot<TodaySections, TodayNewsItem> {
+		var snapshot = NSDiffableDataSourceSnapshot<TodaySections, TodayNewsItem>()
+		snapshot.appendSections([TodaySections.topStories])
+
+		let todayNewsItems = [
+			TodayNewsItem(imageURL: nil, source: "foo", title: "foo", timePublication: "foo"),
+			TodayNewsItem(imageURL: nil, source: "bar", title: "bar", timePublication: "bar"),
+			TodayNewsItem(imageURL: nil, source: "baz", title: "baz", timePublication: "baz"),
+			TodayNewsItem(imageURL: nil, source: "foo", title: "bar", timePublication: "baz")
+		]
+
+		snapshot.appendItems(todayNewsItems)
+		return snapshot
 	}
 }
 
@@ -45,7 +167,7 @@ final class TodayViewController: UIViewController {
 
 import SwiftUI
 
-struct MyProvider: PreviewProvider {
+struct TodayViewControllerProvider: PreviewProvider {
 
 	static var previews: some View {
 		ContainerView().edgesIgnoringSafeArea(.all)
@@ -53,14 +175,14 @@ struct MyProvider: PreviewProvider {
 
 	struct ContainerView: UIViewControllerRepresentable {
 		func makeUIViewController(
-			context: UIViewControllerRepresentableContext<MyProvider.ContainerView>
+			context: UIViewControllerRepresentableContext<TodayViewControllerProvider.ContainerView>
 		) -> TodayViewController {
 			return TodayViewController()
 		}
 
 		func updateUIViewController(
-			_ uiViewController: MyProvider.ContainerView.UIViewControllerType,
-			context: UIViewControllerRepresentableContext<MyProvider.ContainerView>
+			_ uiViewController: TodayViewControllerProvider.ContainerView.UIViewControllerType,
+			context: UIViewControllerRepresentableContext<TodayViewControllerProvider.ContainerView>
 		) { }
 	}
 }
