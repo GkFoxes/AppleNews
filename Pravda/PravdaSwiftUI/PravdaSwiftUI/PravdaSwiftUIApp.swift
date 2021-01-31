e94aefd9c67d84f1df75d7741b3a35a4bdacaf64
GkFoxes <dima26tamys@gmail.com>
2021-01-31 14:31:45 +0300

    PravdaSwiftUI: Replace spaces to tabs

--- Pravda/PravdaSwiftUI/PravdaSwiftUI/PravdaSwiftUIApp.swift
+++ Pravda/PravdaSwiftUI/PravdaSwiftUI/PravdaSwiftUIApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct PravdaSwiftUIApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
